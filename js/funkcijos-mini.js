e541d710efdf5d90844440e62d9dd65c1572b9d7
julijajarmale <julija.jarmale@gmail.com>
2022-04-05 22:39:06 +0300

    pataisymai

--- js/funkcijos-mini.js
+++ js/funkcijos-mini.js
@@ -1,6 +1,6 @@
 // 1. Parašyti funkciją, kuri priima du skaičius. Grąžina tų skaičių sumą.
     
-    function suma(a, b){
+    function suma(a, b) {
         return a + b;
 
     }
@@ -11,7 +11,7 @@
 //2. Parašyti funkciją, kuri priima du kintamuosius. Palygina tuos du skaičius ir grąžina rezultatą ar pirmas skaičius 
 //yra didesnis, ar antras, ar jie yra lygūs.
 
-    function skaiciuAnalize(a, b){
+    function skaiciuAnalize(a, b) {
         if (a > b) {
         return 'Pirmas skaičius yra didesnis';
     }
@@ -23,9 +23,8 @@
         }
         
     }
-     console.log(skaiciuAnalize(3, 4));
-     console.log(skaiciuAnalize(4, 4));
-     console.log(skaiciuAnalize(4, 5));
+     console.log(skaiciuAnalize(3, 3));
+    
    
     
  // 3. Parašyti funkciją, kuri priimtų vieną skaičių. Turi būti grąžinamas rezultatas ar tie metai yra keliemieji.
@@ -80,16 +79,24 @@ console.log(sumaIntervale(6));
 //  6. Parašykite funkciją, kuri priimtų skaičių ir suskaičiuotų, iš kiek sveikų skaičių jos  argumentas dalijasi be
 // liekanos (išskyrus vienetą ir patį save).
 
-function isKiekDalinasi(skaicius1) {
-    let daliklis = 0;
-    for (let i = 0; i < skaicius1; i++) {
-        if ((skaicius1 % i ===0) && (i !== 1)) {
-        daliklis += i;
+    function sveikuSkaicuKiekis(skaicius1) {
+        if (typeof skaicius1 !== 'number'){
+            return 'ERROR: kintamasis nėra skaičius';
+        }
+        if (!isFinite(skaicius1)) {
+            return 'ERROR: kintamasis turi būti tikras skaicius';
+        }
+            let count = 0;
+
+        for (let i=-skaicius1; i<skaicius1; i++){
+            if ((skaicius1 % i ===0) && (i !== 1)) {
+                count++;
+            }
+        }
+        return `Skaičius ${skaicius1} dalijasi iš ${count} sveikų skaičių be liekanos (išskyrus vienetą ir patį save).`
     }
 
-    } return `Skaičius ${skaicius1} dalijasi iš ${daliklis} sveikų skaičių be liekanos (išskyrus vienetą ir patį save).`
-}
-    console.log(isKiekDalinasi(8));
+        console.log(sveikuSkaicuKiekis(9));
 
 
  //   7. Parašyti funkciją, kuri priimtų vieną kintamąjį - tekstą. Funkcija turi išvesti tekstą į ekraną ir dar papildomai
@@ -102,6 +109,22 @@ function isKiekDalinasi(skaicius1) {
 
         console.log(tekstas('Amelija'));
 
+//   8. Parašyti funkciją telefonoNumeris, kuri priima vieną kintamąjį – masyvą su 10 skaičių jame. 
+ //  Funkcija turi grąžinti telefono numerį tokiu formatu - "(XXX) XXX-XXXX". 
+ function telefonoNumeris(number) {
+    if (typeof number !== 'object') {
+        return 'ERROR: neteisingas numeris'
+    }
+    if (number.length !== 10) {
+        return 'ERROR: Netinkamas skaitmenų kiekis'
+    }
+    for (let i = 0; i <= number.length; i++) {
+         return `(${number[0]} ${number[1]} ${number[2]}) ${number[3]} ${number[4]} ${number[5]} - ${number[6]} ${number[7]} ${number[8]} ${number[9]}`;
+        }
+}
+
+console.log(telefonoNumeris([3, 7, 0, 6, 8, 6, 3, 9, 3, 2]));
+console.log(telefonoNumeris([3, 7, 0, 6, 8, 6, 3, 9, 3]));
 
  //    9. Parašyti funkciją, kuri priimtų du kintamuosius - tekstus. Palyginti kuris tekstas yra ilgesnis.
         function tekstoAnalize(tekstas1, tekstas2) {
@@ -136,18 +159,19 @@ function isKiekDalinasi(skaicius1) {
         console.log(tekstoAnalize(2, 'Vakaras'));
 
 
-        
- //     //    8. Parašyti funkciją telefonoNumeris, kuri priima vieną kintamąjį – masyvą su 10 skaičių jame. 
- //    Funkcija turi grąžinti telefono numerį tokiu formatu - "(XXX) XXX-XXXX". 
-     
  //10. Parašyti funkciją, kuri priimtų vieną kintamąjį - tekstą. Suskaičiuoti, kiek tekste yra ‚a‘ raidžių. 
+        function raidziuAnalize(text) {
+            if (typeof text !== 'string') {
+                return 'ERROR: Tekstas yra netinkamo tipo.';
+            }
+                let countA = 0;
+                for (let i=0; i<text.length; i++){
+                    if((text[i] === 'a') || (text[i] === 'A')) {
+                        countA++;
+                    }
+                }
+            return `Tekste ${text} yra ${countA} "a" raidžių`
+        }
         
-       // function raidziuAnalize(text) {
-       //     for (let i = 0; i <= text.length; i++) {
-       //         if ((skaicius1 % i ===0) && (i !== 1)) {
-       //         daliklis += i;
-//
-       // return`Tekste ${text} yra X a raidžių`
-       // }
-       // 
-       // console.log(raidziuAnalize(Labas));
+        console.log(raidziuAnalize('Abrakadabra'));
+        
